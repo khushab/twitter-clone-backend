9c4b6eefb3bd1605733a0b775e6bc2aa50f0cc97
khushab <khushabalam7@gmail.com>
2021-09-13 15:59:42 +0530

    setup complete

---
+++ db-setup.js
@@ -0,0 +1,11 @@
+const knex = require('knex');
+const knexfile = require('./db/knexfile')
+const { Model } = require('objection');
+
+function dbSetup() {
+    const db = knex(knexfile.development);
+    Model.knex(db)
+}
+
+module.exports = dbSetup;
+
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const dbSetup = require('./db-setup')
+const Users = require('./db/models/users')
+const app = express()
+dbSetup()
+
+app.use(express.json())
+app.use(cors())
+
+// Routes----
+app.get('/users', async (req, res) => {
+    const users = await Users.query()
+    res.send(users)
+})
+
+app.listen(3200, () => {
+    console.log('Server is running on port 3200')
+})
